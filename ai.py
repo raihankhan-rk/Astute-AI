9462e3eb5540ae32119b4613ee526c49285fa605
Raihan Khan <iamrk98@gmail.com>
2023-01-02 00:44:05 +0530

    removed all routes and redirected the submit route to the home page

--- ai.py
+++ ai.py
@@ -9,22 +9,20 @@ app.config['SECRET_KEY'] = os.environ.get('app_secret_key')
 openai.api_key = os.environ.get('open_ai_api_key')
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        resp = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=3300,
+            temperature=0.9,
+        )
+        resp["choices"][0]["text"] = resp["choices"][0]["text"].replace('\n', '<br>')
+        return render_template('index.html', response=resp["choices"][0]["text"])
+
     return render_template("index.html", response="")
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    prompt = request.form['prompt']
-    resp = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        max_tokens=3300,
-        temperature=0.9,
-    )
-    resp["choices"][0]["text"] = resp["choices"][0]["text"].replace('\n', '<br>')
-    return render_template('index.html', response=resp["choices"][0]["text"])
 
 
 if __name__ == '__main__':
