c76fc13a9028802969c41ccb60efbb4c10d50f39
Raihan Khan <iamrk98@gmail.com>
2023-01-14 21:55:47 +0530

    removed unnecessary api route

--- ai.py
+++ ai.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 import openai
 
 app = Flask(__name__)
-CORS(app)
 load_dotenv()
 app.config['SECRET_KEY'] = os.environ.get('app_secret_key')
 openai.api_key = os.environ.get('open_ai_api_key')
@@ -25,12 +23,6 @@ def home():
         return render_template('index.html', response=resp["choices"][0]["text"])
 
     return render_template("index.html", response="")
-
-@app.route("/api", methods=["GET"])
-@cross_origin()
-def api():
-    data = {"status": "success", "message": {"name": "Raihan Khan", "isPresent": True}}
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
